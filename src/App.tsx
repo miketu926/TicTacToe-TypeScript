2fcd16bcf6cd2b8bc6040a2f2f0acfb8be63d3ac
Mike <miketu926@gmail.com>
2019-07-10 00:04:20 -0400

    Created a 3x3 board with mouse hover highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+enum Player {
+  None = 0,
+  One = 1,
+  Two = 2
+}
+
+interface IState {
+  board: Player[]
+}
+
+// class App extends React.Component <{}, IState> {
+
 const App: React.FC = () => {
+
+  const [board, setBoard] = useState([Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None, Player.None])
+  const [nextPlayerTurn, setNextPlayerTurn] = useState(Player.One)
+
+  const handleClick = (index: number) => {
+
+    const newBoard = board.slice();
+    newBoard[index] = nextPlayerTurn;
+
+    setBoard(newBoard);
+    setNextPlayerTurn(3 - nextPlayerTurn);
+  }
+
+  const renderCell = (index: number) => {
+    return <div className="cell" onClick={() => handleClick(index)} />
+  }
+
+  const renderBoard = () => {
+    return <div className="board-container">{board.map((cell, i) => renderCell(i))}</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Tic Tac Toe using TypeScript By Mike Tu
         </p>
         <a
           className="App-link"
@@ -19,6 +52,9 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+
+      {renderBoard()}
+
     </div>
   );
 }
